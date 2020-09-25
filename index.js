1ec0157ae434aac96fece4520df973d1600182d9
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-25 14:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten() {
   return kittens.shift()
 }
+
+function appendKitten(name) {
+  b = [name, ...kittens]
+  return b
+}
+
+function prependKitten(name) {
+  b = [kittens, ...name]
+  return b
+}
+
+function removeLastKitten() {
+  return array.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return array.slice(1,kittens.length)
+}
