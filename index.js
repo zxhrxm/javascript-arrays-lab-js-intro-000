7964a806159e321262d68cf9625802d4544314a8
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-25 14:26:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
